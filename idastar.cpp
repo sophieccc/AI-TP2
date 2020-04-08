bf395671fc77eab844aa127ce252354524636bc0
Sophie Crowley <crowleso@tcd.ie>
2020-04-08 13:20:14 +0100

    Setup

---
+++ idastar.cpp
@@ -0,0 +1,256 @@
+/*
+Copyright 2018 Pierre-Edouard Portier
+peportier.me
+​
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+​
+    http://www.apache.org/licenses/LICENSE-2.0
+​
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+)
+*/
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <queue>
+#include <map>
+#include <utility>
+#include <list>
+#include <functional>
+#include <cmath>
+#include <algorithm>
+#include <utility>
+#include <iomanip>
+#include <limits>
+#include <chrono>
+
+using namespace std;
+
+typedef vector<int> State;
+
+//  .---.
+//  |2|0|
+//  .---. -> State b = {2,0,1,3}
+//  |1|3|
+//  .---.
+
+typedef pair<int,int> Move;
+
+typedef function<int( const State& pos )> Heuristic;
+
+int 
+side( const State& b )
+{
+  double y = sqrt( b.size() );
+  int x = y;
+  return x;
+}
+
+int 
+manh( const State& b )
+{
+  int d = 0;
+  int s = side(b);
+  for( int i = 0 ; i < b.size() ; i++ )
+  {
+    if( b[i] != 0 )  // not a tile, '0' doesn't count
+    {
+      d += abs( i / s - b[i] / s ) +
+           abs( i % s - b[i] % s );
+    }
+  }
+  return d;
+}
+
+int
+nbmis( const State& b )
+{
+  int d = 0;
+  for( int i = 0 ; i < b.size() ; i++ )
+  {
+    if( b[i] != 0 ) // not a tile, '0' doesn't count
+    {
+      if( b[i] != i ) d++;
+    }
+  }
+  return d;
+}
+
+bool
+finalState( const State& b )
+{
+  return (nbmis(b) == 0); // we use nbmis for it is quick to compute
+}
+
+void
+print( const State& state )
+{
+  int s = side(state);
+  for( int i = 0 ; i < state.size() ; i++ )
+  {
+    if( i % s == 0 ) cout << endl;
+    cout << setw(2) << setfill('0') << state[i] << " , ";
+  }
+  cout << endl;
+}
+
+void
+doMove( State &state, const Move &move )
+{
+  swap( state[move.first] , state[move.second] );
+}
+
+void
+addNeighbor( State &currentState, Move &move, 
+             vector< pair< Move,int > > &neighbors,
+             list<State>& path, Heuristic h )
+{
+  doMove( currentState, move );
+  if( find( path.begin(), path.end(), currentState ) == path.end() )
+  {
+    neighbors.push_back( make_pair( move, h(currentState) ) );
+  }
+  doMove( currentState, move ); // undo move
+}
+
+void
+search( State& currentState,
+        int          ub, // upper bound over which exploration must stop
+        int&         nub,
+        list<State>& path,
+        list<State>& bestPath,
+        Heuristic    h,
+        int&         nbVisitedState )
+
+{
+  nbVisitedState++;
+
+  int f; // under-estimation of optimal length
+  int g = path.size() - 1; // size of the current path to currentState
+
+  if( finalState( currentState ) )
+  {
+    bestPath = path;
+    return;
+  }
+
+  // generate the neighbors
+  int s = side( currentState );
+  vector< pair< Move,int > > neighbors;
+  neighbors.clear();
+  
+  int pos0 = find( currentState.begin(), currentState.end(), 0 ) - currentState.begin();
+  
+  if( (pos0 + 1) < currentState.size() &&
+      ((pos0 + 1) % s) != 0 )
+  {
+    Move move = make_pair( pos0, pos0 + 1 );
+    addNeighbor( currentState, move, neighbors, path, h );
+  }
+  
+  if( (pos0 - 1) >= 0 &&
+      ((pos0 - 1) % s) != (s-1) )
+  {
+    Move move = make_pair( pos0, pos0 - 1 );
+    addNeighbor( currentState, move, neighbors, path, h );
+  }
+  
+  if( (pos0 + s) < currentState.size() )
+  {
+    Move move = make_pair( pos0, pos0 + s );
+    addNeighbor( currentState, move, neighbors, path, h );
+  }
+  
+  if( (pos0 - s) >= 0 )
+  {
+    Move move = make_pair( pos0, pos0 - s );
+    addNeighbor( currentState, move, neighbors, path, h );
+  }
+
+  // sort the neighbors by heuristic value
+
+  sort( neighbors.begin(), neighbors.end(),
+      [](const pair<Move,int> &left, const pair<Move,int> &right) 
+      {
+        return left.second < right.second;
+      } );
+
+
+  for( const pair<Move,int> &p : neighbors )
+  {
+    f = g + 1 + p.second;
+    if( f > ub )
+    {
+      if( f < nub )
+      {
+        nub = f; // update the next upper bound
+      }
+    }
+    else
+    {
+      doMove( currentState, p.first );
+      path.push_back( currentState );
+      search( currentState, ub, nub, path, bestPath, h, nbVisitedState );
+      path.pop_back();
+      doMove( currentState, p.first ); // undo move
+      if( !bestPath.empty() ) return;
+    }
+  }
+}
+
+void
+ida( State&        initialState, 
+     Heuristic     h,
+     list<State>&  bestPath, // path from source to destination
+     int&          nbVisitedState )
+{
+  int ub;                      // current upper bound
+  int nub = h( initialState ); // next upper bound
+  list<State> path;
+  path.push_back( initialState ); // the path to the target starts with the source
+
+  while( bestPath.empty() && nub != numeric_limits<int>::max() )
+  {
+    ub = nub;
+    nub = numeric_limits<int>::max();
+
+    cout << "upper bound: " << ub;
+    search( initialState, ub, nub, path, bestPath, h, nbVisitedState );
+    cout << " ; nbVisitedState: " << nbVisitedState << endl;
+  }
+}
+
+int
+main()
+{
+  //State b = {11,5,12,14,15,2,0,9,13,7,6,1,3,10,4,8}; // hard
+  //State b = {15,2,12,11,14,13,9,5,1,3,8,7,0,10,6,4};
+  //State b = {10,0,2,4,5,1,6,12,11,13,9,7,15,3,14,8}; // 33 -> 59
+  //State b = {14,1,9,6,4,8,12,5,7,2,3,0,10,11,13,15}; // 35 -> 45
+  //State b = {7,11,8,3,14,0,6,15,1,4,13,9,5,12,2,10}; // C1 36 -> 46
+  //State b = {14,10,9,4,13,6,5,8,2,12,7,0,1,3,11,15}; // C2 43 -> 59
+  State b = {4,8,3,2,0,7,6,5,1}; //C0
+  //State b = {3,2,5,4,1,8,6,7,0};
+  //State b = {1,0,3,4,2,6,7,5,8};
+  list<State> bestPath;
+  int nbVisitedState = 0;
+  
+  auto start = std::chrono::high_resolution_clock::now();
+  ida( b, nbmis, bestPath, nbVisitedState );
+  auto finish = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> elapsed = finish - start;
+  cout << "Elapsed time: " << elapsed.count() << " s\n";
+  cout << "nb moves: " << bestPath.size()-1 << endl;
+  cout << "nb visited states: " << nbVisitedState << endl;
+
+  for( const State& s : bestPath ) print(s);
+  
+  return 0;
+}
---
+++ state.cpp
@@ -0,0 +1,187 @@
+using namespace std;
+class State{
+public:
+    
+    State(int n, int k);
+    // Constructor that creates a state with n blocs and k stacks
+    
+    State(const State& s);
+    // Constructor that creates a copy of s
+    
+    void setInitial();
+    // Initialise this with the initial state (blocs uniformly distributed on stacks)
+    
+    bool isFinal();
+    // Return true if this is a final state (all blocs on the last stack, from largest to smallest)
+    
+    typedef pair<int,int> Move;
+    // A move is a pair of stacks: the bloc on top of the first stack is moved to the top of the second stack
+
+    void doMove(Move m);
+    // Precondition: 0 <= m.first < getNbStacks(), 0 <= m.second < getNbStacks(), and !emptyStack(m.first)
+    // Postrelation: Apply the move (m.first->m.second) to this
+    
+    int heuristic();
+    // Return a lower bound on the minimal number of moves necessary to transform this into the final state
+    
+    void display() const;
+    // Display this
+    
+    bool operator<(const State&) const;
+    bool operator>(const State&) const;
+    bool operator==(const State&) const;
+    bool operator!=(const State&) const;
+    
+    int getNbStacks();
+    // Return the number of stacks
+    
+    bool emptyStack(int s);
+    // Precondition: 0 <= s < getNbStacks()
+    // Return true if stack s has no block
+    
+    ~State();
+    // Destructor
+    
+private:
+    int nbBlocs, nbStacks;
+    int* stack; // For each bloc b: stack[b] = stack that contains b
+    int* next;  // For each bloc b: if b is the lowest block of its stack,
+                // then next[b]=-1, else next[b]=bloc immediately below b
+    int* top;   // For each stack s: if s is empty, then top[s]=-1
+                // else top[s]=bloc on top of s
+    int compare(const int*, const int*, const int*) const; // function to compare states
+};
+
+State::State(int n, int k){
+    // Constructor that creates a state with n blocs and k stacks
+    nbBlocs = n;
+    nbStacks = k;
+    stack = new int[nbBlocs];
+    next = new int[nbBlocs];
+    top = new int[nbStacks];
+}
+
+State::State(const State& state){
+    // Constructor that creates a copy of state
+    nbBlocs = state.nbBlocs;
+    nbStacks = state.nbStacks;
+    stack = new int[nbBlocs];
+    next = new int[nbBlocs];
+    top = new int[nbStacks];
+    for (int s=0; s<nbStacks; s++){
+        top[s] = state.top[s];
+    }
+    for (int b=0; b<nbBlocs; b++){
+        next[b] = state.next[b];
+        stack[b] = state.stack[b];
+    }
+}
+
+State::~State(){
+    delete[] next;
+    delete[] stack;
+    delete[] top;
+}
+
+void State::setInitial(){
+    // Initialise this with the initial state (blocs uniformly distributed on stacks)
+    for (int s=0; s<nbStacks; s++){
+        top[s] = -1;
+    }
+    for (int b=0; b<nbBlocs; b++){
+        int s = b % nbStacks; // Put bloc b on top of stack s
+        stack[b] = s;
+        next[b] = top[s];
+        top[s] = b;
+    }
+}
+
+bool State::isFinal(){
+    // Return true if this is a final state (all blocs on the last stack, from largest to smallest)
+    for (int s=0; s<nbStacks-1; s++){
+        if (top[s] != -1) return false;
+    }
+    if (top[nbStacks-1] != 0) return false;
+    for (int b=0; b<nbBlocs-1; b++){
+        if (stack[b] != nbStacks-1) return false;
+        if (next[b] != b+1) return false;
+    }
+    if (next[nbBlocs-1] != -1) return false;
+    return true;
+}
+
+void State::doMove(Move m){
+    // Precondition: 0 <= m.first < getNbStacks(), 0 <= m.second < getNbStacks(), and !emptyStack(m.first)
+    // Apply the doMove (s1->s2) to this
+    int b1 = top[m.first];
+    int bb1 = next[b1];
+    int b2 = top[m.second];
+    next[b1] = b2;
+    top[m.second] = b1;
+    top[m.first] = bb1;
+    stack[b1] = m.second;
+}
+
+void displayBlockStack(int b, int* next){
+    // Display all blocs below b (including b), from bottom to top
+    if (b >= 0){
+        displayBlockStack(next[b], next);
+        cout << b << " ";
+    }
+}
+
+void State::display() const{
+    // Display this
+    for (int s=0; s<nbStacks; s++){
+        cout << "stack "<< s << ": ";
+        displayBlockStack(top[s], next);
+        cout << endl;
+    }
+}
+
+int State::getNbStacks(){
+    return nbStacks;
+}
+
+bool State::emptyStack(int s){
+    // Precondition: 0 <= s < getNbStacks()
+    // Return true if stack s has no block
+    return top[s] == -1;
+}
+
+bool State::operator<(const State& state) const{
+    // Return true if this < state
+    return state.compare(stack,top,next) < 0;
+}
+
+bool State::operator>(const State& state) const{
+    // Return true if this > state
+    return state.compare(stack,top,next) > 0;
+}
+
+bool State::operator==(const State& state) const{
+    // Return true if this == state
+    return state.compare(stack,top,next) == 0;
+}
+
+bool State::operator!=(const State& state) const{
+    // Return true if this != state
+    return state.compare(stack,top,next) != 0;
+}
+
+int State::compare(const int* stack2, const int* top2, const int* next2) const{
+    for (int b=0; b<nbBlocs; b++){
+        if (stack[b]<stack2[b]) return -1;
+        if (stack[b]>stack2[b]) return 1;
+        if (next[b]<next2[b]) return -1;
+        if (next[b]>next2[b]) return 1;
+    }
+    for (int s=0; s<nbStacks; s++){
+        if (top[s]<top2[s]) return -1;
+        if (top[s]>top2[s]) return 1;
+    }
+    return 0;
+}
+
+
+
